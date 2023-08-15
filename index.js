7955d4679fbd614921ddaf5264932c1fc822fd2f
festuskayan <kayanfestus@gmail>
2023-08-15 12:18:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function turnOn(){
+    document.getElementById("electricity").src="images/greenbulb.png"
+}
+function turnoff(){
+    document.getElementById("electricity").src="images/whitebulb.png"
+}
